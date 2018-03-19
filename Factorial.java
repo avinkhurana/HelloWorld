a96b5bafb39fbce8d58e6b2bf2412569eeff1601
avinkhurana <avin.khurana18@gmail.com>
2018-03-18 20:37:08 -0500

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -4,20 +4,9 @@ import java.util.Scanner;
 
 public class Factorial {
 	
-	public static int FirstFactorial(int num) { 
-		  
-	       int a = 1;
-	       for(int i=0;i<num;i++){
-	           a=a*(num-i);
-	          
-	       }
-	    return a;
-	    
-	  } 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner s = new Scanner(System.in);
-		System.out.print(FirstFactorial(s.nextInt())); 
+		
+		System.out.print("Hello World"); 
 		
 	}
 
